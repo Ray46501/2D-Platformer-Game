0efd3b9a8440c1d82482772a54aae49351107452
Ray46501 <raiyan.rahman1@education.nsw.gov.au>
2024-04-14 13:58:20 +1000

    Fixed Wallslide

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
-#Importing Gravity and Making Speed and JV Accessible in INspector Menu
-
+# Importing Gravity and Making Speed and JV Accessible in Inspector Menu
 @export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+@export var JUMP_VELOCITY = -500.0
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Limiting Player Jumps
+# Handling Wallslide and Limiting Player Jumps
 var current_jumps = 0
 var max_jumps = 2
-const wall_slide_acceleration = 5
-const max_wall_slide_speed = 120
+var wall_jump_rebound = 100
+var wall_slide_gravity = 100
+var is_wall_sliding = false
+
+# Adds Physics
 
 func _physics_process(delta):
 	# Adds Gravity.
@@ -18,20 +21,43 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		if velocity.y > 1000:
 			velocity.y = 1000
-		
-	# Handles Wall SLide and Double Jump
+	
+	wall_slide(delta)    
+
+	if Input.is_action_just_pressed("smash"):
+		velocity.y = -JUMP_VELOCITY * 10
+
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * SPEED            
+
+	# Handles Double and Wall Jump
 	if is_on_floor():
 		current_jumps = 0
-
-	if Input.is_action_just_pressed("jump") && current_jumps < max_jumps:
-		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("jump") && (current_jumps < max_jumps || is_wall_sliding):
+		if is_on_wall() && Input.is_action_pressed("move_right"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x += wall_jump_rebound
+		elif is_on_wall() && Input.is_action_pressed("move_left"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x += -wall_jump_rebound
+		else:
+			velocity.y = JUMP_VELOCITY
 		current_jumps += 1
-		
-	if Input.is_action_just_pressed("smash"):
-		velocity.y = -JUMP_VELOCITY	* 10
-		
-	var direction = Input.get_axis("move_left", "move_right")
-	velocity.x = direction * SPEED
+	
+func wall_slide(delta):
+	if is_on_wall() && !is_on_floor():
+		if Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right"):
+			is_wall_sliding = true
+		else:
+			is_wall_sliding = false    
+	else:
+		is_wall_sliding = false        
+	
+	if is_wall_sliding:
+		current_jumps = 0
+		velocity.y += (wall_slide_gravity * delta)    
+		velocity.y = min(velocity.y, wall_slide_gravity)
 
 	move_and_slide()
 	print(velocity)
